ece480c3ca399bac16cc3b847e7381179698445f
OLEVANDR55 <83229879+OLEVANDR55@users.noreply.github.com>
2023-05-07 09:23:23 +0300

    Add particle during death

--- CurseOfKnight/Assets/Scripts/Enemy/Ghost/GhostEnemy.cs
+++ CurseOfKnight/Assets/Scripts/Enemy/Ghost/GhostEnemy.cs
@@ -24,27 +24,24 @@ public class GhostEnemy : MonoBehaviour
 
     private void Update()
     {
-        // Движение призрака
-        if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        if(player.position == null)
         {
             transform.position = this.transform.position;
         }
-        else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-        if(transform.position.x > player.position.x)
-        {
-            enemySpriteRender.flipX = true;
-        }
         else
         {
-            enemySpriteRender.flipX = false;
+            // Движение призрака
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+                transform.position = this.transform.position;
+            else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+
+            if (transform.position.x > player.position.x)
+                enemySpriteRender.flipX = true;
+            else
+                enemySpriteRender.flipX = false;
         }
 
         // Стрельба по рыцарю
